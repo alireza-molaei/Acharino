b1e3533795c7a84799daf3301166ad7060f93c86
alireza-molaei <alirezamolaei3013@gmail.com>
2023-06-07 06:10:18 +0430

    insert-breeze-authentication

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AboutController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\FaqsController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -49,3 +50,15 @@ Route::get('admin/panel', function () {
 Route::get('faqs/show', [FaqsController::class, 'show'])->name('faqs.show');
 Route::get('/faqs/{faq}/edit', [FaqsController::class, 'edit'])->name('faqs.edit');
 Route::post('/faqs/{faq}', [FaqsController::class, 'update'])->name('faqs.update');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__ . '/auth.php';
