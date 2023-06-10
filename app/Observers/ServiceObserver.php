d3b65e5670c7829656cc46e1d05714a4ef3db704
alireza-molaei <alirezamolaei3013@gmail.com>
2023-06-10 17:38:04 +0430

    repair-git-sheet

---
+++ app/Observers/ServiceObserver.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Service;
+use Illuminate\Support\Facades\Storage;
+
+class ServiceObserver
+{
+    /**
+     * Handle the Service "created" event.
+     */
+    public function created(Service $service): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Service "updated" event.
+     */
+    public function updated(Service $service): void
+    {
+        Storage::delete($service->getOriginal('thumbnail'));
+    }
+
+    /**
+     * Handle the Service "deleted" event.
+     */
+    public function deleted(Service $service): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Service "restored" event.
+     */
+    public function restored(Service $service): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Service "force deleted" event.
+     */
+    public function forceDeleted(Service $service): void
+    {
+        //
+    }
+}
